2aa5e755b2a850737231e2254c86cb5c35ba8c93
umeshgrg29 <umeshgrg29@gmail.com>
2023-06-01 07:53:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+"use strict";
+const num1Element = document.getElementById("num1");
+const num2Element = document.getElementById("num2");
+const buttonElement = document.querySelector('button');
+const numResults = [];
+const textResults = [];
+function add(num1, num2) {
+    if (typeof num1 === 'number' && typeof num2 === 'number') {
+        return num1 + num2;
+    }
+    else if (typeof num1 === 'string' && typeof num2 === 'string') {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
+}
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
+buttonElement === null || buttonElement === void 0 ? void 0 : buttonElement.addEventListener('click', () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    numResults.push(result);
+    const Stringresult = add(num1, num2);
+    textResults.push(Stringresult);
+    printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('It worked!');
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result.split('w'));
+});
